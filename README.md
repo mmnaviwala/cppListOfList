96bdac9e0801ff2405c12812f0c37f4568a30efe
Muhammad <majidnaviwala@gmail.com>
2013-06-29 22:28:49 -0500

    Added cpp file and modified ReadMe file

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-cppListOfList
-=============
+List of list in C++
+===================
 
-Linked list of linked list in C++.
+* This program demonstrates how linked list of linked list work in C++
+* No STL classes have been used in this program.
+* As a result the program displays numbers from 0-99, where numbers 0-9 are part
+of first list and then each number has its own list which is 'that number + 10' 
+* (Compiled this program with Borland C++ compiler)
+
+
+Output of the program:
+=======================
+
+0->10->20->30->40->50->60->70->80->90<br>
+|<br>
+V<br>
+1->11->21->31->41->51->61->71->81->91<br>
+|<br>
+V<br>
+2->12->22->32->42->52->62->72->82->92<br>
+|<br>
+V<br>
+3->13->23->33->43->53->63->73->83->93<br>
+|<br>
+V<br>
+4->14->24->34->44->54->64->74->84->94<br>
+|<br>
+V<br>
+5->15->25->35->45->55->65->75->85->95<br>
+|<br>
+V<br>
+6->16->26->36->46->56->66->76->86->96<br>
+|<br>
+V<br>
+7->17->27->37->47->57->67->77->87->97<br>
+|<br>
+V<br>
+8->18->28->38->48->58->68->78->88->98<br>
+|<br>
+V<br>
+9->19->29->39->49->59->69->79->89->99<br>
---
+++ listOflist.cpp
@@ -0,0 +1,131 @@
+/* * * * * * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * *
+* Author: Muhammad Naviwala
+* Date: 06/29/2013
+* Description: This program demonstrates how linked list of linked list work.
+  No STL classes have been used in this program.
+  As a result the program displays numbers from 0-99, where numbers 0-9 are part
+  of first list and then each number has its own list which is 'that number + 10' 
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+// declaring second node
+// declaring second node first, because its needed to be part of the first node
+class nodeB
+{
+public:
+	int j;
+	nodeB *nextB;
+};
+
+// declaring first node
+class nodeA
+{
+public:
+	int i;
+	nodeA *nextA;
+	nodeB *nextAB;
+};
+
+// Global pointer for nodeA. This will serve as head of the list
+nodeA *head1;
+
+int main()
+{
+	// ========================================================================================
+	// using n1 and n2 to traverse the list
+	nodeA *n1;
+	nodeB *n2;
+	// ========================================================================================
+
+	// ========================================================================================
+	// initializing the list (at this point only variables and pointers were "declared")
+	head1 = new nodeA;
+	head1->nextA = 0;	// setting to NULL
+	n1 = head1;			// from this point using n1 to traverse through the list
+	// ========================================================================================
+
+
+
+	// ========================================================================================
+	for(int x=0; x<10; x++)
+	{
+		// going to the current pointer place
+		if ( n1 != 0 ) 
+		{
+			while ( n1->nextA != 0)
+				n1 = n1->nextA;
+		}
+
+		n1->nextA = new nodeA;
+		n1 = n1->nextA;
+		n1->nextA = 0;
+
+		n1->i = x;
+
+
+		// ----------------------------------------------------------------------------------------
+		n1->nextAB = new nodeB;
+		n1->nextAB->nextB = 0;
+		n2 = n1->nextAB;
+		// ----------------------------------------------------------------------------------------
+
+		for(int y=x; y<100; y=y+10)
+		{
+
+			// ----------------------------------------------------------------------------------------
+			// going to the current pointer place
+			if ( n2 != 0 ) 
+			{
+				while ( n2->nextB != 0)
+					n2 = n2->nextB;
+			}
+
+			n2->nextB = new nodeB;
+			n2 = n2->nextB;
+			n2->nextB = 0;
+
+			n2->j = y;
+			// ----------------------------------------------------------------------------------------
+		}
+	}
+	// ========================================================================================
+
+	// ========================================================================================
+	// now for printing the list
+	nodeA *print1;
+	nodeB *print2;
+
+	print1 = head1;
+	print1 = print1->nextA;
+
+	cout << endl;
+	while ( print1 != NULL ) 
+	{
+		print2 = print1->nextAB;
+		print2 = print2->nextB;
+
+		while( print2 != NULL)
+		{
+			cout << print2->j;
+		
+			if(print2->nextB != NULL)
+			{
+				cout << "->";
+			}
+		
+			print2 = print2->nextB;
+		}
+
+		if(print1->nextA != NULL)
+		{
+			cout << endl << "|" << endl << "V" << endl;
+		}
+
+		print1 = print1->nextA;
+	}
+	cout << endl;
+	// ========================================================================================
+}
